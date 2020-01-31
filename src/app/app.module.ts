646519063bec6083db5f37c4d06d3e50dcff5016
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-01-31 14:16:32 +0000

    added auth module, register component and not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from './auth/auth.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    AuthModule,
     AppRoutingModule
   ],
   providers: [],
