a7769b0f9d0867527205c549d2f73bd0a2440564
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-02-17 15:45:21 +0000

    added state manager for new questionnaire

--- src/app/stores/models/new-questionnaire.models.ts
+++
@@ -1,4 +0,0 @@
-export interface NewQuestionnaireState {
-  email: string;
-  agreed: boolean;
-}
---
+++ src/app/stores/new-questionnaire.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+import { BehaviorSubject } from 'rxjs';
+
+import { Map } from 'immutable';
+
+import { NewQuestionnaireState } from './models/new-questionnaire.models';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewQuestionnaireService {
+
+  private readonly stateDataSubject = new BehaviorSubject<Map<string, any>>(Map({
+    email: '',
+    agreed: false
+  }));
+
+  readonly state = this.stateDataSubject.asObservable();
+
+  private get stateData() {
+    return this.stateDataSubject.getValue();
+  }
+
+  private set stateData(val: Map<string, any>) {
+    this.stateDataSubject.next(val);
+  }
+
+  set email(email: string) {
+    this.stateData = this.stateData.set('email', email);
+  }
+
+  set agreed(agreed: boolean) {
+    this.stateData = this.stateData.set('agreed', agreed);
+  }
+}
