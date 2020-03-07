d73fdc0b0508e834cdd5993498e115135680b6e1
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-03-07 11:46:21 +0000

    created checkbox and radio template

--- src/app/questionnaire/questionnaire.module.ts
+++ src/app/questionnaire/questionnaire.module.ts
@@ -7,10 +7,17 @@ import { QuestionnaireRoutingModule } from './questionnaire-routing.module';
 import { NewQuestionnaireComponent } from './new-questionnaire/new-questionnaire.component';
 import { FillOutQuestionnaireComponent } from './fill-out-questionnaire/fill-out-questionnaire.component';
 import { FreeTextQuestionComponent } from './questions/free-text-question/free-text-question.component';
+import { RadioQuestionComponent } from './questions/radio-question/radio-question.component';
+import { CheckboxQuestionComponent } from './questions/checkbox-question/checkbox-question.component';
 
 
 @NgModule({
-  declarations: [NewQuestionnaireComponent, FillOutQuestionnaireComponent, FreeTextQuestionComponent],
+  declarations: [
+    NewQuestionnaireComponent,
+    FillOutQuestionnaireComponent,
+    FreeTextQuestionComponent,
+    RadioQuestionComponent,
+    CheckboxQuestionComponent],
   imports: [
     CommonModule,
     QuestionnaireRoutingModule,
