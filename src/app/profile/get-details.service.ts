1b02ea459d9b32a7f5e57f0bf0917565b35a5bec
SiobhanHughes <siobhanmahughes@gmail.com>
2020-03-10 17:51:59 +0000

    started get-details service

---
+++ src/app/profile/get-details.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetDetailsService {
+
+  constructor(private http: HttpClient,
+    private router: Router) { }
+}
--- src/app/profile/view-profile/view-profile.component.ts
+++ src/app/profile/view-profile/view-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { GetDetailsService} from '../get-details.service';
+
 @Component({
   selector: 'app-view-profile',
   templateUrl: './view-profile.component.html',
@@ -9,7 +11,7 @@ import { Router } from '@angular/router';
 export class ViewProfileComponent implements OnInit {
   details = ['Dr Siobhan Hughes', 'River place Health Center', 'Essex Road', 'Islington', 'London', 'N1 2DE'];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, getDetailsService: GetDetailsService) { }
 
   ngOnInit(): void {
   }
