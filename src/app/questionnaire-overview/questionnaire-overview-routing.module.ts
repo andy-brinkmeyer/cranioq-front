53f5d5b834546b01485c3a2f20af21a881ce4878
Yasmin Abedin <yasmin_abedin@hotmail.co.uk>
2020-04-02 19:12:35 +0100

    started questionnaire overview page

--- src/app/questionnaire-overview/questionnaire-overview-routing.module.ts
+++ src/app/questionnaire-overview/questionnaire-overview-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { QuestionnaireOverviewComponent } from './questionnaire-overview.component';
 
+
 const routes: Routes = [{ path: 'questionnaire-overview', component: QuestionnaireOverviewComponent}];
 
 
--- src/app/questionnaire-overview/questionnaire-overview.module.ts
+++ src/app/questionnaire-overview/questionnaire-overview.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { QuestionnaireOverviewRoutingModule } from './questionnaire-overview-routing.module';
 import { QuestionnaireOverviewComponent } from './questionnaire-overview.component';
 
+// test questionnaire data 
+import {questionnaires} from '../questionnaires';
+
+
 
 @NgModule({
   declarations: [QuestionnaireOverviewComponent],
