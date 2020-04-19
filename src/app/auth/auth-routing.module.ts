4bd9e7164d3c95dea5cb1b882da2322780bb25d4
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-04-19 20:59:43 +0200

    fixed some canActivate guards and deleted unused component

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,14 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {AuthGuard} from './guards/auth.guard';
 
-import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import {ChangePasswordComponent} from './change-password/change-password.component';
 
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
   { path: 'change-password', component: ChangePasswordComponent, canActivate: [AuthGuard] }
