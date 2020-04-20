ac0fd5f497896128d9b9e0086adbafd7ccaab785
Yasmin Abedin <yasmin_abedin@hotmail.co.uk>
2020-04-20 19:06:18 +0100

    routed questionnaire overview page to specific questionnaires

---
+++ src/app/questionnaire-overview/questionnaire-details/questionnaire-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questionnaire-details',
+  templateUrl: './questionnaire-details.component.html',
+  styleUrls: ['./questionnaire-details.component.css']
+})
+export class QuestionnaireDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
