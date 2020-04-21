67d6133e97c39ccb3a59ba411c9f9d5aa51d6e87
SiobhanHughes <siobhanmahughes@gmail.com>
2020-04-21 18:38:03 +0100

    start notifications service with http client unit testing

--- src/app/navbar/notifications.service.spec.ts
+++ src/app/navbar/notifications.service.spec.ts
@@ -1,9 +1,23 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { NotificationsService } from './notifications.service';
 
 describe('NotificationsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ]
+  }));
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
 
   it('should be created', () => {
     const service: NotificationsService = TestBed.get(NotificationsService);
