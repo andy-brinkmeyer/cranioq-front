6400630a42ec58a6cd9856447e9940a83feeed8b
SiobhanHughes <siobhanmahughes@gmail.com>
2020-04-22 18:29:17 +0100

    notifications service tests for get and put requests and errors

--- src/app/navbar/notifications.service.spec.ts
+++ src/app/navbar/notifications.service.spec.ts
@@ -33,9 +33,8 @@ describe('NotificationsService', () => {
     expect(service).toBeTruthy();
   });
 
-  // Test that expects a GET Request to occur and provides a mock response
-
-  it('getQuestionnaires should provide list of questionnaire ids', () => {
+  // Test that expects a GET Request to occur and provides a mock response - list of questionnaire ids
+  it('getQuestionnaires() should provide a list of questionnaire ids via GET request', () => {
     const listQs: NotificationsResponse200[] = [{id: 1}, {id: 2}, {id: 7}]; /* mock list of questionnaire ids*/
 
     service.getQuestionnaires().subscribe((data: NotificationsResponse200) => {
@@ -43,14 +42,69 @@ describe('NotificationsService', () => {
       expect(JSON.stringify(data)).toEqual(JSON.stringify(listQs));
     });
     const req = httpTestingController.expectOne(environment.apiBaseUrl + '/quests/notify');
-
-    // Respond with mock data, causing Observable to resolve.
-    // Subscribe callback asserts that correct data was returned.
+    expect(req.request.method).toBe('GET');
+    // Respond with mock list of questionnaire ids
     req.flush(listQs);
   });
 
-  /*it('should be created', () => {
-    const service: NotificationsService = TestBed.get(NotificationsService);
-    expect(service).toBeTruthy();
-  });*/
+  // Test 403 error from GET request
+  it('getQuestionnaires() test for 403 error', () => {
+    const emsg = 'deliberate 403 error';
+
+    service.getQuestionnaires().subscribe(data =>
+    fail('should have failed with the 403 error'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(403, 'status');
+        expect(error.error).toEqual(emsg, 'message');
+      }
+    );
+    const req = httpTestingController.expectOne(environment.apiBaseUrl + '/quests/notify');
+    expect(req.request.method).toBe('GET');
+    // Respond with mock error
+    req.flush(emsg, { status: 403, statusText: 'Forbidden' });
+  });
+
+  // Test that expects PUT request to occur - update to dismiss notitifcation
+  it('remove(id) should update dismiss notification to be True via PUT request', () => {
+    service.remove(8).subscribe((data: any) => {
+        expect(data.dismiss).toBe(true);
+      });
+    const req = httpTestingController.expectOne(environment.apiBaseUrl + '/quests/notify/8',
+    'put to api');
+    expect(req.request.method).toBe('PUT');
+    req.flush({dismiss: true});
+  });
+
+  // Test 400 error from PUT request
+  it('remove(id) test for 400 error', () => {
+    const emsg = 'deliberate 400 error';
+
+    service.remove(8).subscribe(data =>
+    fail('should have failed with the 400 error'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toEqual(400, 'status');
+        expect(error.error).toEqual(emsg, 'message');
+      }
+    );
+    const req = httpTestingController.expectOne(environment.apiBaseUrl + '/quests/notify/8',
+    'put to api');
+    expect(req.request.method).toBe('PUT');
+    // Respond with mock error
+    req.flush(emsg, { status: 400, statusText: 'Bad Request' });
+  });
+
+  it('updateData() should emitt updated list of questionnaire ids', () => {
+    const listQs: NotificationsResponse200[] = [{id: 1}, {id: 2}, {id: 7}];
+    // subscribe, but skip the first as this is a BehaviorSubject and emits a default first value.
+    service.updateData().subscribe((data: NotificationsResponse200) => {
+      expect(data).not.toBe(null);
+      expect(JSON.stringify(data)).toEqual(JSON.stringify(listQs));
+    });
+
+    const req = httpTestingController.expectOne(environment.apiBaseUrl + '/quests/notify');
+    expect(req.request.method).toBe('GET');
+    // Respond with mock list of questionnaire ids
+    req.flush(listQs);
+  });
+
 });
